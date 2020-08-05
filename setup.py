561349c546d85127657e46e81100c130277e381a
Duke Lee <dongyeopdukelee@gmail.com>
2020-08-04 22:12:55 -0700

    Add setup.py. Rename

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='discordgifbot',
+	version='0.1',
+	url='https://github.com/dongyeopdukelee/VideoToGifForDiscord',
+	packages=['discordgifbot'],
+	zip_safe=False)
+
